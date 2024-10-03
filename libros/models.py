60f8fc5f4754c5fb58913551dc92809b70bdeb4e
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-10-02 23:55:48 -0600

    Se define el modelo de Libro

--- libros/models.py
+++ libros/models.py
@@ -7,6 +7,12 @@ class Autor(models.Model):
     def __str__(self):
         return self.nombre  # Representación en cadena del autor
 
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)   # Título del libro
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)  # Relación con el modelo Autor
+    fecha_publicacion = models.DateField()      # Fecha de publicación del libro
 
+    def __str__(self):
+        return self.titulo  # Representación en cadena del libro
 
 
