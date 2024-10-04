543bb2c201303373debfad16a0d34cda4b4aeca7
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-10-04 01:29:14 +0000

    Se agrega ejercicio: contador palabras

--- contadorPalabras.py
+++ contadorPalabras.py
@@ -5,9 +5,9 @@ def contar_palabras(archivo):
             contenido = f.read()  # Leer el contenido del archivo
             palabras = contenido.split()  # Dividir el contenido en palabras usando split()
             return len(palabras)  # Contar la cantidad de palabras
-    except FileNotFoundError:
+    except FileNotFoundError: # Si se genera el error FileNotFoundError ejecuta el script
         print(f'El archivo {archivo} no fue encontrado.')
-        return 0
+        return 0 #Al no encontrar el archivo retorna 0
 
 # Solicitar el nombre del archivo al usuario
 archivo = input('Ingresa el nombre del archivo de texto: ')
